a75c6c601a23c9b02b0a3f47eda460568c1f0009
Randy Nguyen <Randywins1993@gmail.com>
2021-01-26 18:51:56 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Randy  Nguyen on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
